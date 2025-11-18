e0bcbb6750db083d0d0b0c9fc31a655d571b5642
zawstudio <webzawneun@gmail.com>
2025-11-18 19:07:02 +0000

    feat: FluentValidation with pipeline behavior

---
+++ SmartBudgetAPI.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SmartBudgetAPI.Application;
+
+/// <summary>
+/// Dependency Injection configuration for Application layer
+/// </summary>
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
+
