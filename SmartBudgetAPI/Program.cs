cfbafb374bce90ad7334a32cbde3447f9e6e3368
zawstudio <webzawneun@gmail.com>
2026-03-14 03:14:08 +0000

    fix: Security-Headers

--- SmartBudgetAPI/Program.cs
+++ SmartBudgetAPI/Program.cs
@@ -123,6 +123,17 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+// Add Security Headers
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Append("X-Frame-Options", "DENY");
+    context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+    context.Response.Headers.Append("Referrer-Policy", "no-referrer");
+    context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none';");
+    await next();
+});
+
 app.UseHttpsRedirection();
 
 app.UseCors("DefaultPolicy");
